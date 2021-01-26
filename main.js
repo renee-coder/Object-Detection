87081cd1b46cbbd670adde78e3c30eed92e7229b
renee-coder <71918527+renee-coder@users.noreply.github.com>
2021-01-26 20:28:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 img = "";
+status = "";
 
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectinig Objects";
 }
 
 function preload(){
  img= loadImage('dog_cat.jpg');
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
 }
 
 function draw(){
@@ -16,4 +32,10 @@ function draw(){
     noFill();
     stroke("purple");
     rect( 30, 60, 450, 350);
+
+    fill("cyan");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("cyan");
+    rect(300, 90, 270, 320); 
 }
